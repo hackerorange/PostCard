da5a6ca9e87dad1e64f9263e183597e86793af5a
ZhongChongtao <2593364535@qq.com>
2017-02-26 19:44:11 +0800

    add json

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace WindowsFormsApplication1
 {
@@ -24,7 +26,50 @@ namespace WindowsFormsApplication1
             {
                 MessageBox.Show("你点击了确定按钮");
                 MessageBox.Show("HelloWorld");
+                Orange orange=new Orange();
+                orange.Name = "王涛";
+                orange.Age = "jasklfdj";
+                String a = JsonConvert.SerializeObject(orange);
+                JsonSerializer serializer = new JsonSerializer();
+                StringReader stringReader = new StringReader(a);
+                Orange o =serializer.Deserialize(new JsonTextReader(stringReader), typeof(Orange)) as Orange;
+                MessageBox.Show(o.Name);
+                MessageBox.Show(o.Age);
+
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Orange
+    {
+        public Orange(String name, String age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public Orange()
+        {
+
+        }
+
+        private String name;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private String age;
+
+        public String Age
+        {
+            get { return age; }
+            set { age = value; }
         }
     }
 }
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string url = string.Format(@"http://webservice.36wu.com/DimensionalCodeService.asmx/GetCodeImgByString?size={0}&content={1}", 5, 123456);
+            System.Net.WebRequest request = System.Net.WebRequest.Create(url);
+            System.Net.WebResponse response = request.GetResponse();
+
+            try {
+                pictureBox1.Image = Image.FromStream(response.GetResponseStream());
+            }catch(Exception){
+            }
+        }
+    }
+}
